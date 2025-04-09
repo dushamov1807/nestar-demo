7b285efba8a93d4c3a4f645b09f3ff25cd9e94b5
dushamovakbar <akbardushamov@Akbarui-MacBookAir.local>
2025-04-09 19:00:07 +0900

    feat: meta tags integrations on document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,30 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log("DOCUMENT - pages Routing");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="Nestar Demo" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="img/png" href="/img/logo/favicon.svg" />
+
+        {/* SEO */}
+
+        <meta
+          name="keyword"
+          content={"nestar, nestar.uz, devex mern, mern nestjs fullsatck"}
+        />
+
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere anytime in South Korea. | " +
+            "Покупайте и продавайте недвижимость в любом месте и в любое время в Южной Корее. | " +
+            "한국에서 언제 어디서나 부동산을 사고팔 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
