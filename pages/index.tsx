974c0c3d561f5891cb355bdc55fc3df12de3382b
dushamovakbar <akbardushamov@Akbarui-MacBookAir.local>
2025-04-18 15:35:49 +0900

    feta: develop apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,30 @@ import TopAgents from "@/libs/components/homepage/TopAgents";
 
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+  console.log("getPropertiesData =>", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>HOMEPAGE MOBILE</Stack>;
